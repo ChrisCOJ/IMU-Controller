47fb128730a22417dcb0926d63e126c36c406e71
ChrisCOJ <catalin.cojocaru0914@gmail.com>
2025-03-15 14:22:14 +0000

    Normalized accelerometer values to pixels / second values and fixed output

--- main/main.c
+++ main/main.c
@@ -16,9 +16,10 @@
 
 #define MPU6050_I2C_ADDR                0x68          // I2C address of the MPU6050 sensor
 
-#define MPU6050_PWR_MGMT_REG            0x6B          
-#define MPU6050_RESET_SIG               0x80
-#define MPU6050_SLEEP_SIG               0x40
+#define MPU6050_PWR_MGMT1_REG           0x6B  
+#define MPU6050_WAKE_UP_SIG             0x00        
+#define MPU6050_RESET_BIT               7
+#define MPU6050_SLEEP_BIT               6
 
 #define MPU6050_ACCEL_REG               0x3B          // The address of the first MPU6050's accelerometer register
 #define MPU6050_ACCEL_CONFIG_REG        0x1c          // Bits 4 and 3 are used to set the maximum value of the accelerometer outputs
@@ -72,27 +73,34 @@ void master_init(int slave_addr) {
 
 
 void mpu_init() {
-    // Reset the MPU6050 registers to their default values
-    mpu_reg_write_byte(dev_handle, MPU6050_PWR_MGMT_REG, MPU6050_RESET_SIG);
+    // Wake up the MPU6050
+    mpu_reg_write_byte(dev_handle, MPU6050_PWR_MGMT1_REG, MPU6050_WAKE_UP_SIG);
 
     // Configure the MPU6050 accelerometer 
     mpu_reg_write_byte(dev_handle, MPU6050_ACCEL_CONFIG_REG, MPU6050_ACCEL_8G);
 }
 
 
-void readAccel(){
+void mpu_read_accel(int16_t *reader_array, size_t reader_array_size){
     /**  Function that reads acceleration data from an MPU6050 over I2C  **/
+    if (sizeof(reader_array_size) < 3) {  // Error handling, reader_arrat size must be >= 3
+        ESP_LOGE("mpu_read_accel()", "Invalid reader_array parameter size. Size must be >= 3");
+        abort();
+    }
 
     // Write the address of the fisrt accelerometer address and read the next 6 bytes into accel_data
     uint8_t accel_data[6];
     esp_err_t err = mpu_read_reg(dev_handle, MPU6050_ACCEL_REG, accel_data, sizeof(accel_data));
 
     if (err == ESP_OK) {
-        int16_t accel_x = (accel_data[0] << 8) | accel_data[1];
-        int16_t accel_y = (accel_data[2] << 8) | accel_data[3];
-        int16_t accel_z = (accel_data[4] << 8) | accel_data[5];
+        int16_t accel_x = ((accel_data[0] << 8) | accel_data[1]);
+        reader_array[0] = accel_x;
+        int16_t accel_y = ((accel_data[2] << 8) | accel_data[3]);
+        reader_array[1] = accel_y;
+        int16_t accel_z = ((accel_data[4] << 8) | accel_data[5]);
+        reader_array[2] = accel_z;
 
-        ESP_LOGI(TAG, "Acceleration X: %d Y: %d Z: %d", accel_x, accel_y, accel_z);
+        // ESP_LOGI(TAG, "Acceleration X: %d Y: %d Z: %d", accel_x, accel_y, accel_z);
     }
 }
 
@@ -100,25 +108,40 @@ void readAccel(){
 void app_main(void)
 {
     // Setup
-    master_init(MPU6050_I2C_ADDR);
-    mpu_init();
+    master_init(MPU6050_I2C_ADDR);  // Init i2c bus, master device, and specified slave device
+    mpu_init();  // Wake up mpu sensor and configure it's registers
 
-    // Read the value of the mpu6050's accel config register
-    uint8_t accel_config_read[1];
-    esp_err_t err = mpu_read_reg(dev_handle, MPU6050_ACCEL_CONFIG_REG, accel_config_read, sizeof(accel_config_read));
+    /** Gyro and Accelerometer values are divided by this ratio and converted to
+    mouse sensitivity (pixels per second) **/
+    uint16_t mouse_sens_ratio = 1000;
+
+    // Read the value of the mpu6050's accel config register to ensure correct initialization
+    uint8_t accel_config_buf[1];
+    esp_err_t err = mpu_read_reg(dev_handle, MPU6050_ACCEL_CONFIG_REG, accel_config_buf, sizeof(accel_config_buf));
 
     if (err == ESP_OK) {
-        ESP_LOGI(TAG, "Value of accel config register: %u", accel_config_read[0]);
+        ESP_LOGI(TAG, "Value of accel config register: %u", accel_config_buf[0]);
     }
 
-    // // Release i2c resources
+    // This array will hold the raw 16-bit acceleration values for the X, Y, and Z axes.
+    int16_t raw_accel_arr[3];
+    size_t raw_accel_arr_size = sizeof(raw_accel_arr) / sizeof(raw_accel_arr[0]);
+    char *axes[3] = { "X", "Y", "Z" };
+    // Loop
+    while(10) {
+        // Reads the MPU6050's raw acceleration values into raw_accel_arr
+        mpu_read_accel(raw_accel_arr, raw_accel_arr_size);
+
+        // Normalize raw acceleration values(signed int16) to pixels per second values based on mouse_sens_ratio and log them.
+        for (int i=0; i < raw_accel_arr_size; ++i) {
+            raw_accel_arr[i] /= mouse_sens_ratio;
+            ESP_LOGI(TAG, "%s: %d", axes[i], raw_accel_arr[i]);
+        }
+        ESP_LOGI(TAG, "");
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+
+    // Release i2c resources
     // i2c_master_bus_rm_device(&dev_handle);
     // i2c_del_master_bus(&mst_bus_handle);
-
-    // Loop
-    while(1) {
-        readAccel();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    
 }
